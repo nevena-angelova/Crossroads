ea106b9921ef6dd42ea7266cdb41e08f2ac82748
nevena-angelova <nev.angelova@gmail.com>
2014-11-15 23:14:54 +0200

    Automapper applied

--- Source/Crossroads/Crossroads.Web/Global.asax.cs
+++ Source/Crossroads/Crossroads.Web/Global.asax.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Crossroads.Web.Infrastructure.Mapping;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -16,6 +18,12 @@ namespace Crossroads.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
+
+            var autoMapperConfig = new AutoMapperConfig(Assembly.GetExecutingAssembly());
+            autoMapperConfig.Execute();
         }
     }
 }
