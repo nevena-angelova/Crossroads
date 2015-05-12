a12db4caac60edbdf49c7ee6cf2b584f017a1dbd
nevena-angelova <nev.angelova@gmail.com>
2015-05-12 20:44:17 +0300

    clear

--- Source/Crossroads/Crossroads.Data.Common/Models/IOrderable.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Crossroads.Data.Common.Models
-{
-    public interface IOrderable
-    {
-        int OrderBy { get; set; }
-    }
-}
--- Source/Crossroads/Crossroads.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Crossroads.Web.MvcApplication" Language="C#" %>
